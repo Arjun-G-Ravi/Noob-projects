633d60255574b35d810e61fb7e734d210f4a1f8a
Arjun <arjungravi1@gmail.com>
2024-02-07 20:42:54 +0530

    more

--- 09_tic_tac_toe/main.py
+++ 09_tic_tac_toe/main.py
@@ -1,8 +1,17 @@
 # Tic-tac-toe using minimax
 
 state = [['' for _ in range(3)] for _ in range(3)]
+# state = [[1,2,3], ['', 4,''], [1,2,'']]
 
-def action(state): return 'states'
+
+def action(state):
+    empty = []
+    
+    for ind1, block in enumerate(state):
+        for ind2, i in enumerate(block):
+            if i == '':
+                empty.append([ind1, ind2])
+    return empty
 
 def transition(state,action): return 'new state'
 
@@ -13,47 +22,61 @@ def utility(state): return 1
 def is_valid(action): return True
 
 
+if __name__ == '__main__':
+    print(action(state))
+
+
+
 def minimax_max(state):
     if terminal_state(state):
         return utility(state), state
     
     actions = action(state)
-    
+    v = 10
     for a in actions:
         new_state = transition(state, a)
-        minimax_min(state)
-        
+        v_, s = minimax_min(state)
+        if v_ < v:
+            v = v_
+            return_state = s
+            
  
 def minimax_min(state):
     if terminal_state(state):
         return utility(state), state
     
     actions = action(state)
-
+    
+    v = -10
     for a in actions:
         new_state = transition(state, a)
         
-        minimax_max(state)
+        v_, s = minimax_max(state)
+        
+        if v_ > v:
+            v = v_
+            return_state = s
+            
         
 
     
     
     
     
-for i in range(5):
-    print(state)
+# for i in range(5):
+#     print(state)
     
-    move = tuple(input('Enter a move'))
-    print(move)
+#     move = tuple(input('Enter a move'))
+#     print(move)
 
-    if not is_valid(move):
-        continue # bug here
+#     if not is_valid(move):
+#         continue # bug here
     
-    state = transition(state, move)
+#     state = transition(state, move)
     
-    # Mini-max
+#     # Mini-max
     
-    minimax_max(state)
+#     minimax_max(state)
     
     
     
