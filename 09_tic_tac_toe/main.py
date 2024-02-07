484bbbef5e6366a1790a99b359437d102329a9e4
Arjun <arjungravi1@gmail.com>
2024-02-07 20:23:24 +0530

    No idea bro

--- 09_tic_tac_toe/main.py
+++ 09_tic_tac_toe/main.py
@@ -12,7 +12,8 @@ def utility(state): return 1
 
 def is_valid(action): return True
 
-def minimax(state):
+
+def minimax_max(state):
     if terminal_state(state):
         return utility(state)
     
@@ -24,6 +25,18 @@ def minimax(state):
         
         minimax(state)
         
+ 
+def minimax_min(state):
+    if terminal_state(state):
+        return utility(state)
+    
+    actions = action(state)
+    max_utility = [(),-10]
+    
+    for a in actions:
+        new_state = transition(state, a)
+        
+        minimax(state)
         
 
     
