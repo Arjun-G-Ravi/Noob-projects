562549f0cb6b653837d68c7b1f7e0f3389a552aa
Arjun-G-Ravi <arjungravi1@gmail.com>
2024-04-22 11:00:23 +0530

    Committed

--- 17_chatbot_gemini/ai.py
+++ 17_chatbot_gemini/ai.py
@@ -19,30 +19,3 @@ class LLM:
         except:
             return 'Failed to fetch data from API'
         return response.text
-
-
-class LLM2:
-    def __init__(self, api_key):
-        # import google.generativeai as genai
-        import os
-        from groq import Groq
-        self.client = Groq(
-            api_key=api_key,
-        )
-
-        
-        
-    
-    def generate(self, inp):
-        '''Generates output using Google API, given the input.'''
-        chat_completion = self.client.chat.completions.create(
-            messages=[
-                {
-                    "role": "user",
-                    "content": "Explain the importance of fast language models",
-                }
-            ],
-            model="mixtral-8x7b-32768",
-        )
-
-        print(chat_completion.choices[0].message.content)
