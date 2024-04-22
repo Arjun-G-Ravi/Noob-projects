846a2cbced16bb4c1e32b176476b472742f5cfa4
Arjun-G-Ravi <arjungravi1@gmail.com>
2024-04-22 11:21:16 +0530

    Committed

--- 18_chatbot_website_groq/ai.py
+++ 18_chatbot_website_groq/ai.py
@@ -1,26 +1,12 @@
-
 class LLM:
     def __init__(self, api_key):
-        # import google.generativeai as genai
         import os
         from groq import Groq
-        self.client = Groq(
-            api_key=api_key,
-        )
+        self.client = Groq(api_key=api_key)
 
-        
-        
-    
     def generate(self, inp):
         '''Generates output using Google API, given the input.'''
         chat_completion = self.client.chat.completions.create(
-            messages=[
-                {
-                    "role": "user",
-                    "content": "Explain the importance of fast language models",
-                }
-            ],
-            model="mixtral-8x7b-32768",
-        )
-
-        print(chat_completion.choices[0].message.content)
+            messages=[{"role": "user","content": "Explain the importance of fast language models"}],
+            model="mixtral-8x7b-32768",)
+        return chat_completion.choices[0].message.content
