e8cf170acff1e798e85036ad5ad63986b94e0cce
Arjun-G-Ravi <arjungravi1@gmail.com>
2024-04-22 10:52:41 +0530

    Committed

--- 17_chatbot_gemini/ai.py
+++ 17_chatbot_gemini/ai.py
@@ -25,13 +25,17 @@ class LLM2:
     def __init__(self, api_key):
         # import google.generativeai as genai
         import os
-
         from groq import Groq
-        client = Groq(
-            api_key=os.environ.get("GROQ_API_KEY"),
+        self.client = Groq(
+            api_key=api_key,
         )
 
-        chat_completion = client.chat.completions.create(
+        
+        
+    
+    def generate(self, inp):
+        '''Generates output using Google API, given the input.'''
+        chat_completion = self.client.chat.completions.create(
             messages=[
                 {
                     "role": "user",
@@ -42,13 +46,3 @@ class LLM2:
         )
 
         print(chat_completion.choices[0].message.content)
-        
-        self.generation_config=genai.types.GenerationConfig(candidate_count=1, max_output_tokens=900, temperature=0.1, top_p=.9)
-    
-    def generate(self, inp):
-        '''Generates output using Google API, given the input.'''
-        try:
-            response = self.model.generate_content(inp+"Answer in one sentences.", generation_config=self.generation_config)
-        except:
-            return 'Failed to fetch data from API'
-        return response.text
